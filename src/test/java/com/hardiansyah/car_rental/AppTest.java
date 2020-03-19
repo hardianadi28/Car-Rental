f4468edc0676a5b654f0aaecd295483ce29e95f7
Hardiansyah <hardiansyah.adi@gmail.com>
2020-03-19 09:12:57 +0700

    commit unit test

--- src/test/java/com/hardiansyah/car_rental/AppTest.java
+++ src/test/java/com/hardiansyah/car_rental/AppTest.java
@@ -44,16 +44,58 @@ public class AppTest
     public void testCase1() {
     	/**
     	 * Corola, price per day: 150.000
-    	 * built in 2005, disc 7%
-    	 * rent duration 3
+    	 * built in 2005, has disc 7%, disc price: 139.500
+    	 * rent duration 3 (plus disc 5%)
     	 * 
-    	 * (150.000*3) = 450.000
-    	 * 450.000 - (450.000*7%) = 418.500
+    	 * (139.500*3) = 418.500
     	 * 418.500 - (418.500*5%) = 397.575
     	 */
-    	RentTransaction trx1 = new RentTransaction(3);
-        trx1.addCar(car3);
+    	RentTransaction trx = new RentTransaction(3);
+        trx.addCar(car3);
         
-    	assertEquals(397575, trx1.getTotalPrice(), 0);
+    	assertEquals(397575, trx.getTotalPrice(), 0);
+    }
+    
+    @Test
+    public void testCase2() {
+    	/**
+    	 * Xenia, 250.000
+    	 * Avanza, 300.000
+    	 * Corola, 150.000, built below 2010, has disc 7%, disc price: 139.500
+    	 * Rent Duration, 2 days
+    	 * 
+    	 * Disc applied, 10% for 3 cars
+    	 * 
+    	 * (250.000+300.000+139.500) * 2 = 1.379.000
+    	 * 1.379.000 - (1.379.000 * 10%) = 1.241.100
+    	 */
+    	RentTransaction trx = new RentTransaction(2);
+    	trx.addCar(car1);
+    	trx.addCar(car2);
+    	trx.addCar(car3);
+    	
+    	assertEquals(1241100, trx.getTotalPrice(),0);
+    }
+    
+    @Test
+    public void testCase3() {
+    	/**
+    	 * Xenia, 250.000
+    	 * Avanza, 300.000
+    	 * Corola, 150.000, built below 2010, has disc 7%, disc price: 139.500
+    	 * Rent Duration, 3 days
+    	 * 
+    	 * Disc applied, 10% for 3 cars. rent for 3 days, disc 5%
+    	 * 
+    	 * (250.000+300.000+139.500) * 3 = 2.068.500
+    	 * 2.068.500 - (2.068.500 * 10%) = ‭1.861.650‬
+    	 * ‭1.861.650‬ - (‭1.861.650‬ * 5%) = ‭1.768.567,5‬
+    	 */
+    	RentTransaction trx = new RentTransaction(3);
+    	trx.addCar(car1);
+    	trx.addCar(car2);
+    	trx.addCar(car3);
+    	
+    	assertEquals(1768567.5, trx.getTotalPrice(),0);
     }
 }
